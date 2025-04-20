236a15869546b75de5b12325dfd4b9c807f661e2
= <=>
2025-04-20 12:50:32 +0530

    UI layout added and bug fixes

--- expense_tracker/tracker/views.py
+++ expense_tracker/tracker/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Expense, Category, CustomUser
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.forms import AuthenticationForm
-from django.http import HttpResponse
+# from django.contrib.auth.forms import AuthenticationForm
+# from django.http import HttpResponse
 
 def user_register(request):
     if request.method == 'POST':
@@ -16,10 +16,14 @@ def user_register(request):
             messages.error(request, 'Username already taken')
         else:
             CustomUser.objects.create(username=username, email=email, password=password)
+            messages.success(request, 'Registered Successfully.')
             return redirect('login')
     return render(request, 'register.html')
 
 def user_login(request):
+    # If the user is already authenticated, redirect to expense-list
+    if request.user.is_authenticated:
+        return redirect('expense-list')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -29,26 +33,42 @@ def user_login(request):
         user = authenticate(request,username = username,password = password)
         if user:
             login(request,user)
+            messages.success(request, 'Logged In Successfully!')
             return redirect('expense-list')
         else:
-            messages.error(request, 'Invalid credentials')
+            messages.warning(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'login.html')
 
 
 @login_required
 def user_logout(request):
-    logout(request)
-    return redirect('login')
+    if request.method == 'POST':
+        logout(request)
+        messages.success(request, "You have been logged out.")
+        return redirect('login')
+    return render(request, 'logout_confirm.html')
 
-@login_required
+def index(request):
+    return render(request,'home.html')
+
 def expense_list(request):
+    # If the user is not logged in, redirect to the login page with a message
+    if not request.user.is_authenticated:
+        messages.warning(request, 'You must be logged in to view your expenses.')
+        return redirect('login')
+    
     expenses = Expense.objects.filter(user=request.user)
-    context = {'expenses' : expenses}
-    return render(request,'expense_list.html',context)
+    context = {'expenses': expenses}
+    return render(request, 'expense_list.html', context)
 
 
-@login_required
+
 def expense_create(request):
+    # If the user is not logged in, redirect to the login page with a message
+    if not request.user.is_authenticated:
+        messages.warning(request, 'You must be logged in to create your expenses.')
+        return redirect('login')
     categories = Category.objects.filter(user=request.user)
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -67,6 +87,7 @@ def expense_create(request):
 
         # Create the expense and associate the category
         Expense.objects.create(user=request.user, title=title, amount=amount, date=date, notes=notes, category=category)
+        messages.success(request, 'Expense Created Successfully!')
         return redirect('expense-list')
     
     return render(request, 'expense_form.html', {'categories': categories})
@@ -95,7 +116,7 @@ def expense_update(request, pk):
         # Update the expense with the selected or new category
         expense.category = category
         expense.save()
-
+        messages.success(request, 'Expense Updated Successfully!')
         return redirect('expense-list')
 
     return render(request, 'expense_form.html', {'expense': expense, 'categories': categories})
@@ -103,8 +124,9 @@ def expense_update(request, pk):
 
 @login_required
 def expense_delete(request, pk):
-    expense = Expense.objects.get(id=pk, user=request.user)
+    expense = get_object_or_404(Expense, id=pk, user=request.user)
     if request.method == 'POST':
         expense.delete()
+        messages.success(request, 'Expense Deleted Successfully!')
         return redirect('expense-list')
-    return render(request, 'core/expense_confirm_delete.html', {'expense': expense})
+    return render(request, 'expense_confirm_delete.html', {'expense': expense})
